66b16bed59e9da97e5f5f3e07ccd7af770956185
Programming-Seungwan <swsj4480@gmail.com>
2025-01-29 22:45:10 +0900

    feat : 애플리케이션을 linkedIn API에 연결
    1. 파이썬의 문법은 블록 단위가 아니라 변수가 모듈, 파일 단위로 살아 있다.
    2. 현재에는 ProxyCurl API의 무료 크레딧에 대한 소모를 막기 위해 mock 데이터의 호출을 이용한다.
    3. nodeJS의 http 모듈처럼 통신을 위해 requests 모듈을 이용한다.

--- seungwan.py
+++ seungwan.py
@@ -4,17 +4,13 @@ from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 
-information = """
-    Elon Reeve Musk (/ˈiːlɒn mʌsk/; born June 28, 1971) is a businessman and political figure known for his key roles in the automotive company Tesla, Inc. and the space company SpaceX. He is also known for his ownership of X Corp. (the company that operates the social media platform X, formerly Twitter), and his role in the founding of the Boring Company, xAI, Neuralink, and OpenAI. Musk is the wealthiest individual in the world; as of January 2025, Forbes estimates his net worth to be US$427 billion.
-
-A member of the wealthy South African Musk family, Musk was born in Pretoria and briefly attended the University of Pretoria. At the age of 18 he immigrated to Canada, acquiring its citizenship through his Canadian-born mother, Maye. Two years later, he matriculated at Queen's University in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University but never enrolled in classes, and with his brother Kimbal co-founded the online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same year, Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In 2002, Musk acquired United States citizenship, and that October eBay acquired PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight services company, in 2002.
-"""
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == '__main__':
     print("Hello langchain")
     load_dotenv()
     summary_template = """
-    given information {information} about a person from I want you to create:
+    given the linkedin information {information} about a person from I want you to create:
     1. a short summary
     2. two interesting facts about them
 """
@@ -22,11 +18,14 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
      # 온도는 모델이 얼마나 창의적일지를 의미. 0은 창의적이지 않음
-    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     # llm = ChatOllama(model="llama3.2")
-    llm = ChatOllama(model="mistral")
+    # llm = ChatOllama(model="mistral")
 
     chain = summary_prompt_template | llm | StrOutputParser()
 
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(linkedin_profile="https://www.linkedin.com/in/eden-marco/", mock=True)
+
+    res = chain.invoke(input={"information": linkedin_data})
+
     print(res)
